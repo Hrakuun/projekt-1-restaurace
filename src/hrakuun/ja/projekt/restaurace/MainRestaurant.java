ba24f5e4fc80f797f36bd17e1a0e68cb3258a89f
Jan Maxa <hrakuun@gmail.com>
2024-03-03 22:44:54 +0100

    # testing data in Main class
    - work in progress

--- src/hrakuun/ja/projekt/restaurace/MainRestaurant.java
+++ src/hrakuun/ja/projekt/restaurace/MainRestaurant.java
@@ -1,9 +1,25 @@
 package hrakuun.ja.projekt.restaurace;
 
+import java.math.BigDecimal;
+
 public class MainRestaurant {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RestaurantException {
 
+        CookBook cookBook = new CookBook();
+        Dish water = new Dish("Minerální voda", BigDecimal.valueOf(34),1);
+        Dish juice = new Dish("džus", BigDecimal.valueOf(50),1);
+
+
+        try{
+            cookBook.loadFile(Settings.getCookBookFilePath());
+            cookBook.addDish(water);
+//            cookBook.addDish(juice);
+//            cookBook.removeDish(water);
+//            cookBook.addDish(water);
+        } catch (RestaurantException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
 
 
     }
