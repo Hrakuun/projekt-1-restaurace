25c34d6a8c652191f4c35eb405a7c15182875b10
Jan Maxa <hrakuun@gmail.com>
2024-03-03 13:43:54 +0100

    # added constructor with id
    - added nextId

--- src/hrakuun/ja/projekt/restaurace/Dish.java
+++ src/hrakuun/ja/projekt/restaurace/Dish.java
@@ -3,7 +3,9 @@ package hrakuun.ja.projekt.restaurace;
 import java.math.BigDecimal;
 
 public class Dish {
-// region variables
+    // region variables
+    private static int nextId = 1;
+    private int id;
     private String title;
     private BigDecimal price;
     private int preparationTime;
@@ -12,20 +14,30 @@ public class Dish {
 //    region methods
 
 
-
-//    endregion
+    //    endregion
 // region constructors
     public Dish(String title, BigDecimal price, int preparationTime, String image) throws RestaurantException {
         this.title = title;
         setPrice(price);
         setPreparationTime(preparationTime);
         setImage(image);
+        setId(nextId);
+        nextId++;
+    }
+
+    public Dish(int id, String title, BigDecimal price, int preparationTime, String image) throws RestaurantException {
+        this.title = title;
+        setPrice(price);
+        setPreparationTime(preparationTime);
+        setImage(image);
+        setId(id);
     }
 
     public Dish(String title, BigDecimal price, int preparationTime) throws RestaurantException {
-        this(title,price,preparationTime, "blank");
+        this(title, price, preparationTime, "blank");
     }
-//    endregion
+
+    //    endregion
 // region get/set
     public String getTitle() {
         return title;
@@ -40,7 +52,7 @@ public class Dish {
     }
 
     public void setPrice(BigDecimal price) throws RestaurantException {
-        if(price.compareTo(BigDecimal.ZERO) <=0){
+        if (price.compareTo(BigDecimal.ZERO) <= 0) {
             throw new RestaurantException("Cena musí být větší než nula!");
         }
         this.price = price;
@@ -51,7 +63,7 @@ public class Dish {
     }
 
     public void setPreparationTime(int preparationTime) throws RestaurantException {
-        if(preparationTime <= 0){
+        if (preparationTime <= 0) {
             throw new RestaurantException("Čas přípravy pokrmu musí být větší než nula!");
         }
         this.preparationTime = preparationTime;
@@ -62,10 +74,18 @@ public class Dish {
     }
 
     public void setImage(String image) throws RestaurantException {
-        if(image.isBlank()){
+        if (image.isBlank()) {
             throw new RestaurantException("Položka musí mít zadán název obrázku!");
         }
         this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 //    endregion
 }
