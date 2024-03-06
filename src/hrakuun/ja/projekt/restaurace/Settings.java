896be6f6d8e030d5a6d38f62fdcd30059147d2cd
Jan Maxa <hrakuun@gmail.com>
2024-03-06 19:50:45 +0100

    removed variables for "completed orders"

--- src/hrakuun/ja/projekt/restaurace/Settings.java
+++ src/hrakuun/ja/projekt/restaurace/Settings.java
@@ -3,18 +3,14 @@ package hrakuun.ja.projekt.restaurace;
 public class Settings {
 
     private static final String COOKBOOKFILE = "dataFiles/cookBook.txt";
-    private static final String ORDERSINPROGRESS = "dataFiles/ordersInProgress.txt";
-    private static final String COMPLETEDORDERS = "dataFiles/completedOrders.txt";
+    private static final String ORDERSFILE = "dataFiles/ordersInProgress.txt";
     private static final String DELIMITER = ";";
 
     public static String getCookBookFilePath(){
         return COOKBOOKFILE;
     }
     public static String getOrdersInProgressPath() {
-        return ORDERSINPROGRESS;
-    }
-    public static String getCompletedOrdersPath(){
-        return COMPLETEDORDERS;
+        return ORDERSFILE;
     }
     public static String getDelimiter() {
         return DELIMITER;
