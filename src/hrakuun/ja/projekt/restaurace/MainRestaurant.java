c0fc06fd0d24d30001bf48f203f891b46575de0d
Jan Maxa <hrakuun@gmail.com>
2024-03-07 16:31:04 +0100

    test data, WIP

--- src/hrakuun/ja/projekt/restaurace/MainRestaurant.java
+++ src/hrakuun/ja/projekt/restaurace/MainRestaurant.java
@@ -7,6 +7,7 @@ public class MainRestaurant {
     public static void main(String[] args) throws RestaurantException {
 
         CookBook cookBook = new CookBook();
+        OrderManager orderManager = new OrderManager();
         Dish water = new Dish("Minerální voda", BigDecimal.valueOf(34),1);
         Dish juice = new Dish("džus", BigDecimal.valueOf(50),1);
 
@@ -17,6 +18,7 @@ public class MainRestaurant {
             cookBook.addDish(juice);
             cookBook.removeDish(water);
             cookBook.addDish(water);
+//            orderManager.addOrder(new Order(water,2,4));
         } catch (RestaurantException e) {
             System.err.println(e.getLocalizedMessage());
         }
