61a43a25baa8a2399dcdefcb1e5780766f90811a
Jan Maxa <hrakuun@gmail.com>
2024-03-10 12:02:34 +0100

    added method for getting totalPriceForTable

--- src/hrakuun/ja/projekt/restaurace/RestaurantManager.java
+++ src/hrakuun/ja/projekt/restaurace/RestaurantManager.java
@@ -1,12 +1,12 @@
 package hrakuun.ja.projekt.restaurace;
 
-import java.time.LocalDateTime;
+import java.math.BigDecimal;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 public class RestaurantManager {
 
-    public int currentOrdersCount() {
+    public static int currentOrdersCount() {
         int currentOrdersCount = 0;
         for (Order order : OrderManager.getAllOrders()) {
             if (order.isCompleted()) {
@@ -16,13 +16,13 @@ public class RestaurantManager {
         return currentOrdersCount;
     }
 
-    public List<Order> ordersSortedByOrderedTime() {
+    public static List<Order> ordersSortedByOrderedTime() {
         List<Order> orders = new ArrayList<>(OrderManager.getAllOrders());
         orders.sort(Comparator.comparing(Order::getOrderedTime));
         return orders;
     }
 
-    public int averageTimeOfOrderFulfilmentInMinutes() {
+    public static double averageTimeOfOrderFulfilmentInMinutes() {
         int averageTime = 0;
         List<Order> orders = new ArrayList<>(OrderManager.getCompletedOrders());
         long totalTime = 0;
@@ -35,19 +35,20 @@ public class RestaurantManager {
         return averageTime;
     }
 
-    public List<Dish> dishesOrderedToday() {
+    public static List<Dish> dishesOrderedToday() {
         List<Order> orders = OrderManager.getTodayOrders();
         return getDishesFromCookBook(getDishIdsFromOrders(orders));
     }
 
-    private List<Dish> getDishesFromCookBook(Set<Integer> dishIds) {
+    private static List<Dish> getDishesFromCookBook(Set<Integer> dishIds) {
         List<Dish> dishesToday = new ArrayList<>();
         for (Integer id : dishIds) {
             dishesToday.add(CookBook.getDishById(id));
         }
         return dishesToday;
     }
-    private Set<Integer> getDishIdsFromOrders(List<Order> orders){
+
+    private static Set<Integer> getDishIdsFromOrders(List<Order> orders) {
         Set<Integer> dishIds = new HashSet<>();
         for (Order order : orders) {
             dishIds.add(order.getDishId());
@@ -55,34 +56,46 @@ public class RestaurantManager {
         return dishIds;
     }
 
-    private String tableNoToString(int tableNo){
+    private static String tableNoToString(int tableNo) {
         String tableNoString = String.valueOf(tableNo);
-        if(tableNoString.length() < 2){
-            tableNoString = " " + String.valueOf(tableNo);
+        if (tableNoString.length() < 2) {
+            tableNoString = " " + (tableNo);
         }
         return tableNoString;
     }
-    private String headlineOfReport(int tableNo){
-        return String.format("** Objednávky pro stůl č. %s **",tableNoToString(tableNo));
+
+    private static String headlineOfReport(int tableNo) {
+        return String.format("** Objednávky pro stůl č. %s **", tableNoToString(tableNo));
     }
-    private String lineOfReport(Order order, int lineCounter){
+
+    private static String lineOfReport(Order order, int lineCounter) {
         return lineCounter + ". " + order.stringOutput() + "\n";
     }
-    private String linesOrdersReport(List<Order> orders){
+
+    private static String linesOrdersReport(List<Order> orders) {
         int lineCounter = 1;
         StringBuilder report = new StringBuilder();
-        for(Order order : orders){
+        for (Order order : orders) {
             report.append(lineOfReport(order, lineCounter));
             lineCounter++;
         }
         return report.toString();
     }
 
-    public String ordersOnTableReport(int tableNo){
+    public static String ordersOnTableReport(int tableNo) {
         List<Order> orders = OrderManager.getOrdersForTable(tableNo);
         String report;
-        report = headlineOfReport(tableNo) + "\n****\n" + linesOrdersReport(orders);
+        report = headlineOfReport(tableNo) + "\n****\n" + linesOrdersReport(orders) + "******";
         return report;
+    }
+
+    public static BigDecimal totalPriceForTable(int tableNo) {
+        List<Order> orders = OrderManager.getOrdersForTable(tableNo);
+        BigDecimal totalPricePerTable = BigDecimal.ZERO;
+        for (Order order : orders) {
+            totalPricePerTable = totalPricePerTable.add(order.totalPrice());
+        }
+        return totalPricePerTable;
     }
 
 }
